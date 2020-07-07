0729dea2eae0a6bd4ce8b1b2e4e94bcee751556e
Florian Langer <fmlanger@gmail.com>
2020-07-07 16:08:18 +0200

    another 100 examples

--- target_pose/play_recording.py
+++ target_pose/play_recording.py
@@ -5,26 +5,28 @@ import speech_recognition as sr
 from scipy.io.wavfile import write
 import wavio
 
+
+
 r = sr.Recognizer()
 
 fs = 44100
-path = os.path.dirname(os.path.realpath(__file__)) + '/training_data'
-myarray = np.load(path + '/recordings/recording_0014.npy')
+path = os.path.dirname(os.path.realpath(__file__)) + '/training_data1'
+myarray = np.load(path + '/recordings/recording_0003.npy')
 print(myarray)
 print(myarray.shape)
 sd.play(myarray, fs)
 sd.wait()
 
 
-wavio.write(path + '/recordings/recording_0014.wav', myarray, fs ,sampwidth=1)
+# wavio.write(path + '/recordings/recording_0014.wav', myarray, fs ,sampwidth=1)
 
 
-harvard = sr.AudioFile(path + '/recordings/recording_0014.wav',)
-with harvard as source:
-    audio = r.record(source)
+# harvard = sr.AudioFile(path + '/recordings/recording_0014.wav',)
+# with harvard as source:
+#     audio = r.record(source)
 
-print(type(audio))
-print(r.recognize_google(audio,language='en-IN',show_all = True))
+# print(type(audio))
+# print(r.recognize_google(audio,language='en-IN',show_all = True))
 
 
 
